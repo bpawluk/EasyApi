93c064c95b6b3c11f2d2b878df097bc50dee80fc
Bartosz <pawlukb@outlook.com>
2024-04-03 12:36:52 +0200

    testing requests with enum params

--- BlazorUtils.EasyApi.Tests/ParamTypes/EnumParamsTests.cs
+++ BlazorUtils.EasyApi.Tests/ParamTypes/EnumParamsTests.cs
@@ -1,8 +1,27 @@
-﻿using BlazorUtils.EasyApi.Tests.SUT.Server;
+﻿using BlazorUtils.EasyApi.Tests.SUT.Contract.ParamTypes;
+using BlazorUtils.EasyApi.Tests.SUT.Server;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace BlazorUtils.EasyApi.Tests.ParamTypes;
 
-internal class EnumParamsTests(WebApplicationFactory<Program> factory) : TestsBase(factory)
+public class EnumParamsTests(WebApplicationFactory<Program> factory) : TestsBase(factory)
 {
+    [Fact]
+    public async Task Request_WithEnumParams()
+    {
+        var request = new EnumParamsRequest()
+        {
+            Enum = Season.Summer,
+            EnumDefault = default,
+            NullableEnumWithValue = Season.Autumn,
+            NullableEnumDefault = null
+        };
+
+        var result = await CallHttp<EnumParamsRequest, EnumParamsRequest.Response>(request);
+
+        Assert.Equal(request.Enum, result.Enum);
+        Assert.Equal(request.EnumDefault, result.EnumDefault);
+        Assert.Equal(request.NullableEnumWithValue, result.NullableEnumWithValue);
+        Assert.Equal(request.NullableEnumDefault, result.NullableEnumDefault);
+    }
 }
