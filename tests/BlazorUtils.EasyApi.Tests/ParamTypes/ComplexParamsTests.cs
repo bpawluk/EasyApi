d7c2573450ec085a9cd0e426ad5cc1b1a12e1186
Bartosz <pawlukb@outlook.com>
2024-04-04 22:54:39 +0200

    testing requests with complex paramx

--- tests/BlazorUtils.EasyApi.Tests/ParamTypes/ComplexParamsTests.cs
+++ tests/BlazorUtils.EasyApi.Tests/ParamTypes/ComplexParamsTests.cs
@@ -1,5 +1,44 @@
-﻿namespace BlazorUtils.EasyApi.Tests.ParamTypes;
+﻿using BlazorUtils.EasyApi.Tests.SUT.Contract.ParamTypes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorUtils.EasyApi.Tests.ParamTypes;
 
 public abstract class ComplexParamsTests(TestsFixture fixture) : TestsBase(fixture)
 {
+    [Fact]
+    public async Task Request_WithComplexParams()
+    {
+        var request = new ComplexParamsRequest()
+        {
+            Struct = new() { Amount = 9.99M, Currency = "USD" },
+            StructDefault = default,
+            NullableStructWithValue = new() { Amount = 1M, Currency = "PLN" },
+            NullableStructDefault = null,
+            Class = new() { Name = "T-Shirt", Price = 9.99, StockQuantity = 100, CreatedAt = DateTime.UtcNow },
+            ClassDefault = null,
+        };
+
+        var result = await CallHttp<ComplexParamsRequest, ComplexParamsRequest.Response>(request);
+
+        Assert.Equal(request.Struct, result.Struct);
+        Assert.Equal(request.StructDefault, result.StructDefault);
+        Assert.Equal(request.NullableStructWithValue, result.NullableStructWithValue);
+        Assert.Equal(request.NullableStructDefault, result.NullableStructDefault);
+        Assert.Equal(request.Class, result.Class);
+        Assert.Equal(request.ClassDefault, result.ClassDefault);
+    }
+}
+
+public class Client_ComplexParamsTests(TestsFixture fixture) : ComplexParamsTests(fixture)
+{
+    protected override ICall<Request> GetCaller<Request>() => _client.Services.GetRequiredService<ICall<Request>>();
+
+    protected override ICall<Request, Response> GetCaller<Request, Response>() => _client.Services.GetRequiredService<ICall<Request, Response>>();
+}
+
+public class Server_ComplexParamsTests(TestsFixture fixture) : ComplexParamsTests(fixture)
+{
+    protected override ICall<Request> GetCaller<Request>() => _server.Services.GetRequiredService<ICall<Request>>();
+
+    protected override ICall<Request, Response> GetCaller<Request, Response>() => _server.Services.GetRequiredService<ICall<Request, Response>>();
 }
