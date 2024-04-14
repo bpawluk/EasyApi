eba3632c8ed0e50ce0b83ee68fc2ec1e00178cba
Bartosz <pawlukb@outlook.com>
2024-04-14 14:35:31 +0200

    testing requests with collection type response

---
+++ tests/BlazorUtils.EasyApi.Tests.SUT.Server/Handlers/ResponseTypes/CollectionResponseRequestsHandler.cs
@@ -0,0 +1,38 @@
+﻿using BlazorUtils.EasyApi.Server;
+using BlazorUtils.EasyApi.Tests.SUT.Contract.Data;
+using BlazorUtils.EasyApi.Tests.SUT.Contract.ResponseTypes;
+
+namespace BlazorUtils.EasyApi.Tests.SUT.Server.Handlers.ResponseTypes;
+
+internal class CollectionResponseRequestsHandler
+    : HandlerBase
+    , IHandle<ArrayResponseRequest, int[]>
+    , IHandle<EnumerableResponseRequest, IEnumerable<Price>>
+    , IHandle<ListResponseRequest, IList<Product>>
+    , IHandle<SetResponseRequest, ISet<float>>
+    , IHandle<StackResponseRequest, Stack<string>>
+    , IHandle<QueueResponseRequest, Queue<Price>>
+    , IHandle<DictionaryResponseRequest, IDictionary<string, Product>>
+{
+    public Task<HttpResult<int[]>> Handle(ArrayResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<int[]>.Ok(request.ExpectedResponse));
+
+    public Task<HttpResult<IEnumerable<Price>>> Handle(EnumerableResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<IEnumerable<Price>>.Ok(request.ExpectedResponse));
+
+    public Task<HttpResult<IList<Product>>> Handle(ListResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<IList<Product>>.Ok(request.ExpectedResponse));
+
+    public Task<HttpResult<ISet<float>>> Handle(SetResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<ISet<float>>.Ok(request.ExpectedResponse));
+
+    public Task<HttpResult<Stack<string>>> Handle(StackResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<Stack<string>>.Ok(request.ExpectedResponse));
+
+    public Task<HttpResult<Queue<Price>>> Handle(QueueResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<Queue<Price>>.Ok(request.ExpectedResponse));
+
+    public Task<HttpResult<IDictionary<string, Product>>> Handle(DictionaryResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(HttpResult<IDictionary<string, Product>>.Ok(request.ExpectedResponse));
+
+}
--- tests/BlazorUtils.EasyApi.Tests/ResponseTests/ResponseTypesTests/ComplexResponseTests.cs
+++ tests/BlazorUtils.EasyApi.Tests/ResponseTests/ResponseTypesTests/ComplexResponseTests.cs
@@ -15,7 +15,6 @@ public abstract class ComplexResponseTests(TestsFixture fixture) : TestsBase(fix
 
     public static TheoryData<Product> ClassValues => new()
     {
-        null!,
         new(),
         new() { Name = "T-Shirt", Price = 9.99, StockQuantity = 100, CreatedAt = DateTime.UtcNow }
     };
@@ -53,22 +52,35 @@ public abstract class ComplexResponseTests(TestsFixture fixture) : TestsBase(fix
 
     [Theory]
     [MemberData(nameof(ClassValues))]
-    public async Task Request_ForResponse_OfClassType(Product expectedResponse)
+    public async Task Request_ForResponse_OfClassType_WithValuee(Product expectedResponse)
     {
         var request = new ClassResponseRequest() { ExpectedResponse = expectedResponse };
         var result = await CallHttp<ClassResponseRequest, Product>(request);
         Assert.Equal(request.ExpectedResponse, result);
     }
+
+    [Fact]
+    public async Task Request_ForResponse_OfClassType_NoValue()
+    {
+        var request = new ClassResponseRequest() { ExpectedResponse = null! };
+        var caller = GetCaller<ClassResponseRequest, Product?>();
+
+        var response = await caller.CallHttp(request, CancellationToken.None);
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.False(response.HasResponse);
+    }
 }
 
-public class Client_ComplexResponseTests(TestsFixture fixture) : TimeResponseTests(fixture)
+public class Client_ComplexResponseTests(TestsFixture fixture) : ComplexResponseTests(fixture)
 {
     protected override ICall<Request> GetCaller<Request>() => _client.Services.GetRequiredService<ICall<Request>>();
 
     protected override ICall<Request, Response> GetCaller<Request, Response>() => _client.Services.GetRequiredService<ICall<Request, Response>>();
 }
 
-public class Server_ComplexResponseTests(TestsFixture fixture) : TimeResponseTests(fixture)
+public class Server_ComplexResponseTests(TestsFixture fixture) : ComplexResponseTests(fixture)
 {
     protected override ICall<Request> GetCaller<Request>() => _server.Services.GetRequiredService<ICall<Request>>();
 
